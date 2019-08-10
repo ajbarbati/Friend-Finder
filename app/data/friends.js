905a69583d21e56a06a57d0c7365e039de910bac
Alex B <ajbarbati@gmail.com>
2019-08-10 17:40:07 -0400

    edited friends and survey

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+var userData = [
+    {
+      name: 'Phil',
+      photo: 'data.png',
+      scores: [
+          1,
+          3,
+          4,
+          5,
+          3,
+          2,
+          1,
+          1,
+          4,
+          5,
+      ]
+    }
+  ];
+  
+  
+  module.exports = userData
